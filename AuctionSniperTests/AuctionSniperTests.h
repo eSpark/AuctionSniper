52ce7a2c0550e2b926b88c3f0037bcba4641663b
John Richardson <john@esparklearning.com>
2013-03-10 13:37:00 -0500

    Initial Commit

---
+++ AuctionSniperTests/AuctionSniperTests.h
@@ -0,0 +1,13 @@
+//
+//  AuctionSniperTests.h
+//  AuctionSniperTests
+//
+//  Created by John Richardson on 3/10/13.
+//  Copyright (c) 2013 eSpark Learning. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AuctionSniperTests : SenTestCase
+
+@end
---
+++ AuctionSniperTests/AuctionSniperTests.m
@@ -0,0 +1,32 @@
+//
+//  AuctionSniperTests.m
+//  AuctionSniperTests
+//
+//  Created by John Richardson on 3/10/13.
+//  Copyright (c) 2013 eSpark Learning. All rights reserved.
+//
+
+#import "AuctionSniperTests.h"
+
+@implementation AuctionSniperTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in AuctionSniperTests");
+}
+
+@end
