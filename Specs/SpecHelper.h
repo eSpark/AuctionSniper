54b8719d434091da0d00fdbf7beb39ab3612003e
John Richardson <john@esparklearning.com>
2013-03-10 13:48:32 -0500

    sets up Cedar specs

---
+++ Specs/SpecHelper.h
@@ -0,0 +1,20 @@
+//
+//  SpecHelper.h
+//  AuctionSniper
+//
+//  Created by John Richardson on 3/10/13.
+//  Copyright (c) 2013 eSpark Learning. All rights reserved.
+//
+
+#ifndef AuctionSniper_SpecHelper_h
+#define AuctionSniper_SpecHelper_h
+
+// ARC has to be disabled for each spec file due to an issue with
+// objective-c++.
+// importing this file will flag an error if ARC is enabled.
+#if __has_feature(objc_arc)
+#error ARC must be disabled for specs!
+#endif
+
+
+#endif
